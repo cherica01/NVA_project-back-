d5fcce41cf06a809c443bad4ff655854fbe57098
Cherica <riantsoacherica01@gmail.com>
2025-03-15 17:15:57 +0300

    naverina le back

--- NVA_project/api/urls.py
+++ NVA_project/api/urls.py
@@ -1,11 +1,15 @@
-from django.urls import path,include
+from django.urls import path, include
 from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
-    # Autres URLs de votre application
+    # URLs pour l'authentification JWT
     path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
-    path('accounts/', include('accounts.urls')),
-    path('management/', include('management.urls')),
     
+    # URLs pour la gestion des agents (déjà existant)
+    path('accounts/', include('accounts.urls')),
+    
+    # Nouvelles URLs pour chaque application
+    path('event/', include('event.urls')),
+   
 ]
